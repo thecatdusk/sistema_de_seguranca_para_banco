b1e0e6e57e0ec8f94d528f00905f14ed5df4fcd2
Lucas Tirone <lucas.tironeee@gmail.com>
2025-06-01 10:59:36 -0300

    teste1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@RestController
+	public class HelloController {
+
+		@GetMapping("/payment")
+		public String payment(){
+
+			return "<h1>Payment Processed Successfully!</h1>";
+		}
+	}
 }
