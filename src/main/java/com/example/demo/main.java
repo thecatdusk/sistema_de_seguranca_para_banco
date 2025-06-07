c660d19714ddff2ea02e0647fb4295eed70192f9
Lucas Tirone <lucas.tironeee@gmail.com>
2025-06-07 10:46:32 -0300

    integrando o endpoint "/payBill" com o front do index.html

--- src/main/java/com/example/demo/main.java
+++ src/main/java/com/example/demo/main.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.example.demo.classes.User;
 import com.example.demo.classes.checkingAccount;
 
@@ -17,5 +16,4 @@ public class main {
 
 	}
 
-	
 }
